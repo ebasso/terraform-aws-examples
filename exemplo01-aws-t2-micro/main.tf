5783634848f5a00cc6c00322a10e64af972250fd
Enio Basso <ebasso@ebasso.net>
2019-02-23 08:59:48 -0300

    update 2019-02-23-02

---
+++ exemplo01-aws-t2-micro/main.tf
@@ -0,0 +1,18 @@
+# Define a região e as credenciais para a AWS
+provider "aws" {
+  region     = "us-east-1"
+  shared_credentials_file = "˜/.aws/credentials"
+}
+
+# Cria uma instancia EC2 - Ubuntu 18.04
+resource "aws_instance" "example01" {
+  ami           = "ami-0080e4c5bc078760e"
+  instance_type = "t2.micro"
+
+}
+
+# Cria uma instancia EC2 - Ubuntu 18.04
+resource "aws_instance" "example02" {
+  ami           = "ami-0080e4c5bc078760e"
+  instance_type = "t2.micro"
+}
